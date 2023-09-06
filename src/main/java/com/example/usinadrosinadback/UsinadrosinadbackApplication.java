b8c874894adbc43922ff0463dcba2e5db73d016b
Peeter Lond <Peeter.Lond@outlook.com>
2023-09-06 16:00:17 +0300

    Initial commit

---
+++ src/main/java/com/example/usinadrosinadback/UsinadrosinadbackApplication.java
@@ -0,0 +1,13 @@
+package com.example.usinadrosinadback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsinadrosinadbackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsinadrosinadbackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/usinadrosinadback/UsinadrosinadbackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.usinadrosinadback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsinadrosinadbackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
